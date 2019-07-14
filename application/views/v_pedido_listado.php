b57e5e692c980cd1678fdb17f7175160f7b3c9cd
jocabrer <jocabrer@gmail.com>
2019-07-13 22:08:57 -0400

    Cambios menor de diseñoi

--- application/views/v_pedido_listado.php
+++ application/views/v_pedido_listado.php
@@ -233,9 +233,9 @@
 		  
 		<?php if($this->ion_auth->is_admin()){ ?>
 			<!-- hojas recientes -->
-			<div class="box box-primary collapsed-box">
+			<div class="box box-primary box">
 				<div class="box-header with-border">
-						<h3 class="box-title">Hojas Recientes</h3>
+						<h3 class="box-title">Últimas hojas</h3>
 						<div class="box-tools pull-right">
 							<button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-plus"></i></button>
 						</div>
