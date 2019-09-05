170e6119de2a06ae3ac0ca3ec60e01739109940f
jocabrer <jocabrer@gmail.com>
2019-09-04 22:57:15 -0400

    Version chatbox 0.1

---
+++ application/controllers/Chatbox.php
@@ -0,0 +1,16 @@
+
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Chatbox extends CI_Controller {
+
+	
+	public function index()
+	{
+        
+        $this->load->view('v_chatbox'); // footer
+	}
+    
+}
+
+
